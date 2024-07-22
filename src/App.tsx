bbfe2c57d896860d9a038239ed8b4fdea7060aca
NGUYỄN THANH SƠN <nguyenthanhson162001@gmail.com>
2024-07-22 15:19:32 +0700

    'feat:-iframe-website-wixsite.com'

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+
+
+  const removeElements = () => {
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <iframe
+      ref={iframeRef}
+      src="https://thaliatran.wixsite.com/home"
+      style={{
+        width: '100vw',
+        height: '100vh',
+        overflow: 'hidden',
+        border: 'none'
+      }}
+      onLoad={removeElements}
+    ></iframe>
   );
 }
 
