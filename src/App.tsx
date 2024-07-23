de22c64ee161eaa1e89fff6fcec0920950c109b5
NGUYỄN THANH SƠN <nguyenthanhson162001@gmail.com>
2024-07-23 17:47:29 +0700

    fix bug phone

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,33 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [iframeSrc, setIframeSrc] = useState('');
+
+  useEffect(() => {
+    // Function to detect mobile device
+    const detectMobile = () => {
+      return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    };
+
+    // Set the iframe src based on whether the user is on a mobile device
+    if (detectMobile()) {
+      setIframeSrc('/portfolio-thalia/homepage.pdf#toolbar=0&scrollbar=0&statusbar=0&view=FitH');
+    } else {
+      alert("This is phone")
+      setIframeSrc('/portfolio-thalia/homepage.pdf');
+    }
+  }, []);
+
   return (
     <iframe
-      src="/portfolio-thalia/homepage.pdf#toolbar=0&scrollbar=0&statusbar=0&view=FitH"
+      src={iframeSrc}
       style={{
-        width: '100vw',
+        width: '100%',
         height: '100vh',
-        overflow: 'hidden',
-        border: 'none'
+        border: 'none',
       }}
     ></iframe>
   );
 }
-
 
 export default App;
