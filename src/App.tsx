3f41c1c890e261ed53f52c3d8e605479a54bbc28
NGUYỄN THANH SƠN <nguyenthanhson162001@gmail.com>
2024-09-05 10:06:53 +0700

    Add GA4 ID

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Homepage from './pages/homepage';
 import ReactGA from 'react-ga4';
-const TRACKING_ID = "";
+const TRACKING_ID = "G-0VH7FMSGB1";
 ReactGA.initialize(TRACKING_ID);
 function App() {
   return (
