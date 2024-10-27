f3a0604e60ac550cd0176b6a9819e7ec5f47f1af
NGUYỄN THANH SƠN <nguyenthanhson162001@gmail.com>
2024-10-27 15:56:31 +0700

    update node_env

--- src/api/constance.ts
+++ src/api/constance.ts
@@ -1,6 +1,6 @@
 export const SERVER_URL =
   process.env.NODE_ENV == "development"
     ? "http://localhost:1337"
-    : "https://cms.thaliatrandesign.com/";
+    : "https://cms.thaliatrandesign.com";
 console.log({ SERVER_URL, NODE_ENV: process.env.NODE_ENV });
 export const API_GET_ABOUT_US = `${SERVER_URL}/api/about-us?populate=*`;
