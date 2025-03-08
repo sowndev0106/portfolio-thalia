710b6820503f9debe16f13ff30531f2b620f027e
sown <thanhson.nguyen@lotte.vn>
2025-03-08 15:56:21 +0700

    update backend url

--- src/api/constance.ts
+++ src/api/constance.ts
@@ -1,5 +1,5 @@
-// export const SERVER_URL = "https://cms.thaliatrandesign.com";
-export const SERVER_URL = "http://localhost:1337";
+export const SERVER_URL = "https://cms.thaliatrandesign.com";
+// export const SERVER_URL = "http://localhost:1337";
 export const API_GET_ABOUT_US = `${SERVER_URL}/api/about-us?populate=*`;
 export const API_GET_LIST_NEWS = `${SERVER_URL}/post-news?populate=*`;
 export const API_GET_NEWS_BY_SLUG = (slug: string) =>
